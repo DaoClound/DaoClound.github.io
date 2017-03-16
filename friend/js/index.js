525692351e0e082bf2ab1beee950f90a33668576
DaoClound <1284615790@qq.com>
2017-03-17 01:39:39 +0800

    friendCode

--- friend/js/index.js
+++ friend/js/index.js
@@ -1,3 +1,27 @@
-$("section .look-review").on("click",function(){
-	alert(2);
-})
+$(function () {
+	var onOff = true;
+	var heartnum = $('section .comment p:eq(0) span').html();
+	$("section .look-review i").on("touchstart",function(){
+		$(this).addClass(function () {
+			if(onOff){
+				$(this).addClass('iconfont icon-jiankuohaoxishang');
+				$('section .hide-review').css('display','block');
+			}else{
+				$(this).removeClass('icon-jiankuohaoxishang');
+				$('section .hide-review').css('display','none');
+			}
+			onOff = !onOff;
+		});
+	});	
+	$('section .comment p:eq(0) i').on("touchstart",function () {
+		if(onOff){
+			$(this).css('background','#5bafff');
+			heartnum++;
+		}else{
+			$(this).css('background','#fff');
+			heartnum--;
+		}
+		onOff = !onOff;			
+	})
+});
+
