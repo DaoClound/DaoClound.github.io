6e268681760b32977c8df57c3e01c775c9926d8e
DaoClound <1284615790@qq.com>
2017-03-17 11:16:15 +0800

    Update index.js

--- friend/js/index.js
+++ friend/js/index.js
@@ -1,6 +1,5 @@
 $(function () {
-	var onOff = true;
-	var heartNum = parseInt($('section .comment p:eq(0) span').html());
+	var onOff = true;	
 	$("section .look-review i").on("touchstart",function(){
 		$(this).addClass(function () {
 			if(onOff){
@@ -14,6 +13,7 @@ $(function () {
 		});
 	});	
 	$('section .comment p:nth-child(1) i').on("touchstart",function () {
+		var heartNum=parseInt($(this).next("span").html());
 		if(onOff){
 			heartNum++;
 			$(this).css('background','#5bafff');
