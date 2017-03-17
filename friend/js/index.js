79eeba7a1627f524cee5969134db6167b1e86c4a
DaoClound <1284615790@qq.com>
2017-03-17 11:47:48 +0800

    Update index.js

--- friend/js/index.js
+++ friend/js/index.js
@@ -1,22 +1,19 @@
 $(function () {
-	var onOff = true;	
-	$("section .look-review i").on("touchstart",function(){
-		$(this).addClass(function () {
-			if(onOff){
-				$(this).addClass('iconfont icon-jiankuohaoxishang');
-				$('section .hide-review').css('display','block');
-			}else{
-				$(this).removeClass('icon-jiankuohaoxishang');
-				$('section .hide-review').css('display','none');
-			}
-			onOff = !onOff;
-		});
+	var onOff = true;
+	var flag=true;
+	$("section .look-review i").on("touchstart",function(){	
+		if(onOff){
+			$(this).addClass('iconfont icon-jiankuohaoxishang');
+			$('section .hide-review').css('display','block');
+		}else{
+			$(this).removeClass('icon-jiankuohaoxishang');
+			$('section .hide-review').css('display','none');
+		}
+		onOff = !onOff;		
 	});	
-	console.log($("section .comment p").eq(0).find("i").length);
 	$("section .comment p").eq(0).find("i").on("touchstart",function () {
 		var heartNum=parseInt($(this).next("span").html());
-		if(onOff){
-			console.log($(this).next("span").length);
+		if(flag){
 			heartNum++;
 			$(this).css('background','#5bafff');
 			$(this).next("span").html(heartNum);
@@ -25,7 +22,7 @@ $(function () {
 			$(this).css('background','#fff');
 			$(this).next("span").html(heartNum);
 		}
-		onOff = !onOff;			
+		flag = !flag;			
 	})
 });
 
